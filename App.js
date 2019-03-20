20dd437f44d0e5148f699585aa4c308564c296f6
Ranielison <ranielisonsoares2@gmail.com>
2019-03-19 22:26:39 -0300

    implementação da logica do jogo

--- App.js
+++ App.js
@@ -1,53 +1,64 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-
-class MeuComponente extends Component {
-    render(){
-      return(
-        <View>
-            <Text>{this.props.teste}</Text>
-      	</View>
-    )
-  }
-} 
-
-
-
-
+import { Estilo } from './src/estilos/Estilos'
 
 export default class App extends Component {
-	  
+	
 	constructor(props){
 		super(props)
 
 		this.state = {
-			texto: 'Texto teste'
+			escolhaUsuario: '',
+			escolhaComputador: '',
+			resultado: ''
 		}
+	}
+
+	opcoesComputador = ['Pedra', 'Papel', 'Tesoura']
+
+	jokenpo = (escolhaUsuario) => {
+		let choice = Math.floor(Math.random() * 3)
+		let escolhaComputador = this.opcoesComputador[choice]
 		
+		let resultado = ''
+
+		if(escolhaUsuario == 'Papel'){
+			if(escolhaComputador == 'Pedra')
+				resultado = 'Usuario ganhou'
+			else if(escolhaComputador == 'Papel')
+				resultado = 'Empate'
+			else
+				resultado = 'Computador ganhou'
+		}else if(escolhaUsuario == 'Pedra'){
+			if(escolhaComputador == 'Pedra')
+				resultado = 'Empate'
+			else if(escolhaComputador == 'Papel')
+				resultado = 'Computador ganhou'
+			else
+				resultado = 'Usuario ganhou'
+		}else{
+			if(escolhaComputador == 'Pedra')
+				resultado = 'Computador ganhou'
+			else if(escolhaComputador == 'Papel')
+				resultado = 'Usuario ganhou'
+			else
+				resultado = 'Empate'
+		}
+		this.setState({escolhaUsuario, escolhaComputador, resultado}) //Só posso deixar apenas o nome da variavel pq o par chave valor tem o mesmo nome
 	}
-	
-	alteratTexto = () => {
-		this.setState( {texto: 'outra coisa'})
-	}
-	
+
+
 	render() {
+		const { container, botao } = Estilo
     	return (
-      		<View style={styles.container}>
-        		<MeuComponente teste={this.state.texto}></MeuComponente>
-				<Button
-					title='Botão'
-					onPress={() => this.alteratTexto()}
-				/>
+      		<View style={container}>
+        		<Text>Escolha do computador: {this.state.escolhaComputador}</Text>
+				<Text>Escolha do usuario: {this.state.escolhaUsuario}</Text>
+				<Text>Resultado: {this.state.resultado}</Text>
+				<Button title='Pedra' onPress={() => {this.jokenpo('Pedra')}}/>
+				<Button title='Papel' onPress={() => {this.jokenpo('Papel')}}/>
+				<Button  title='Tesoura' onPress={() => {this.jokenpo('Tesoura')}}/>
 			</View>
     	);
   	}
 }
-
-const styles = StyleSheet.create({
-  	container: {
-    	flex: 1,
-    	backgroundColor: '#fff',
-    	alignItems: 'center',
-    	justifyContent: 'center',
-  	},
-});
---
+++ src/estilos/Estilos.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+export const Estilo = StyleSheet.create({
+    container: {
+		marginTop: 20 
+		/*flex: 1,
+    	backgroundColor: '#fff',
+    	alignItems: 'center',
+    	justifyContent: 'center',*/
+	},
+    botao: {
+        marginTop: 30,
+        backgroundColor: 'red'
+    }
+})
